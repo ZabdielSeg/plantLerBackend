170e94230bf060516eb8f43def5e31838cb17228
Zabdiel Segura <zabdiel-segura@hotmail.com>
2022-02-28 18:41:57 -0600

    Try with code to set headers

--- app.js
+++ app.js
@@ -83,6 +83,16 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("*", (req, res, next) => {
   // If no routes match, send them the React HTML.
