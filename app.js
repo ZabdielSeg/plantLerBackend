b3c06492a1d8b7e842f943bd1e741a7f0897ee07
Zabdiel Segura <zabdiel-segura@hotmail.com>
2022-02-28 18:51:04 -0600

    changed origin

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(
 
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_POINT);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
